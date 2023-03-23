dfe3f2a0d1b6197e78695b629891b86d0cfebd85
Nicolò <n.berti@mail.com>
2023-03-23 15:56:24 +0100

    upload sito

--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
  
-import  { logIn } from '/file.js';
+import  { logIn } from 'https://divincolato.github.io/src/file.js';
  // Selezioniamo il form di login
  const loginForm = document.getElementById('login-form');
  // Aggiungiamo un event listener per l'evento di submit del form
